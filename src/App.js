de15524a12ca170f30e6539cd3779daa80269c6f
JunfengHan <505065510@qq.com>
2020-04-12 16:42:02 +0800

    添加首页路由

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@
 import React, { Component } from 'react';
 // Provider 将 store 存储的 state 提供给容器
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header'
+import Home from './pages/home';
+import Article from './pages/article';
 import store from './store';
 
 class App extends Component {
@@ -12,6 +15,10 @@ class App extends Component {
     return (
       <Provider store = { store } >
         <Header />
+        <BrowserRouter>
+          <Route path='/' exact component={ Home }></Route>
+          <Route path='/article' exact component={ Article }></Route>
+        </BrowserRouter>
       </Provider>
     );
   }
