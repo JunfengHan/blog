1eedfb885a1d1a8534ee520556f80d3a3651ee30
JunfengHan <505065510@qq.com>
2020-04-15 23:48:07 +0800

    添加接口统一配置文件

---
+++ src/config/api/index.js
@@ -0,0 +1,8 @@
+/*
+ * 接口配置文件，统一在这里配置
+ */
+
+export const ApiTagsList = '/api/tagsList.json';
+export const ApiHomeList = '/api/articleList.json';
+export const ApiArticleDetail = '/api/articleDetail.json';
+export const ApiArticleComment = '/api/articleDetail.json';
