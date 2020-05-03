98764bcf78a41c72aa00944fc42259df29d254be
JunfengHan <505065510@qq.com>
2020-05-03 14:47:58 +0800

    user 接口路径

--- src/config/api/index.js
+++ src/config/api/index.js
@@ -6,3 +6,4 @@ export const ApiTagsList = '/api/tagsList.json';
 export const ApiHomeList = '/api/articleList.json';
 export const ApiArticleDetail = '/api/articleDetail.json';
 export const ApiArticleComment = '/api/articleDetail.json';
+export const ApiUser = 'user';
